1de3a2a10fcd251fb4ea2ea88a3d0861c6d72e25
Robert Forster <robert.forster@beamvex.co.uk>
2026-05-27 20:16:22 +0100

    first commit

---
+++ alias.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Bash aliases
+alias ll="ls -la"
+alias lhrt="ls -lhrta"
+
+alias lm="less"
+
+# apt aliases
+alias aptu="sudo apt update"
+alias aptug="sudo apt upgrade"
+alias apti="sudo apt install"
+alias aptr="sudo apt remove"
+alias aptpurge="sudo apt purge"
+alias aptarem="sudo apt autoremove"
+
+# Git aliases
+alias g="git"
+alias gi="git init -b main"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gs="git status"
+alias ga="git add"
+alias gaa="git add --all"
+alias gc="git commit"
+alias gcam="git commit -am"
+alias gph="git push"
+alias gpu="git push -u origin HEAD"
+alias gp="git pull"
+alias gl="git log"
+
+# Docker aliases
+alias d="docker"
+alias dps="docker ps"
+alias dpa="docker ps -a"
+alias dstop="docker stop"
+alias drm="docker rm"
+alias drmi="docker rmi"
+alias dexec="docker exec"
+alias dlogs="docker logs"
+alias dbuild="docker build"
+alias dcp="docker-compose"
+
+# Python aliases
+alias p="python"
+alias pi="pip3"
+
+# Node aliases
+alias n="node"
+alias ni="npm install"
+alias nid="npm install --save-dev"
+alias nis="npm install --save"
+alias ngi="npm install -g"
+alias ngid="npm install -g --save-dev"
+alias ngis="npm install -g --save"
+alias ns="npm start"
+alias nd="npm run dev"
+alias nr="npm run"
+alias np="npm"
+alias nin="npm init"
+alias y="yarn"
+alias pn="pnpm"
+
+# Terraform/grunt aliases
+alias tf="terraform"
+alias tfi="terraform init"
+alias tfp="terraform plan"
+alias tfa="terraform apply"
+alias tfr="terraform refresh"
+alias tfo="terraform output"
+alias tfs="terraform state"
+alias tft="terraform taint"
+alias tfu="terraform untaint"
+alias tfv="terraform validate"
+alias tfw="terraform workspace"
+alias tfd="terraform destroy"
+alias tfg="terraform graph"
+alias tfs="terraform show"
+alias tft="terraform taint"
+alias tfu="terraform untaint"
+alias tfv="terraform validate"
+alias tfw="terraform workspace"
+alias tfd="terraform destroy"
+alias tfg="terraform graph"
+alias tfs="terraform show"
+
+alias tg="terragrunt"
+
+
+# Other aliases
+alias c="clear"
+
+
